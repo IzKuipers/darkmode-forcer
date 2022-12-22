f6f20b0d4cfd3e60ac46f2f7d05be53b451531b1
Izaak Kuipers <izaak.kuipers@gmail.com>
2022-12-22 08:46:31 +0100

    Cleanup

--- js/content2.js
+++ js/content2.js
@@ -4,7 +4,7 @@ function start() {
     console.log("%cDarkMode Forcer: Walking through all CSS rules in this document...", DMF_CSS);
 
     let count = 0;
-    let trule = 0;
+    let total = 0;
 
     for (let i = 0; i < document.styleSheets.length; i++) {
         const rules = document.styleSheets[i].rules;
@@ -17,8 +17,7 @@ function start() {
                 console.log(`%cDarkMode Forcer: Found media query: StyleSheets[${i}][${j}]`, DMF_CSS);
 
                 const nStr = `@media screen {${cStr.split("{\n")[1].trim().split(";").join("!important;")}`;
-
-                let ss = document.createElement("style");
+                const ss = document.createElement("style");
 
                 ss.innerText = nStr;
                 document.body.append(ss);
@@ -26,11 +25,11 @@ function start() {
                 count++;
             }
 
-            trule++
+            total++;
         }
     }
 
-    console.log(`%cDarkMode Forcer: Done! ${count}/${trule} rules out of ${document.styleSheets.length} stylesheets processed.`, DMF_CSS);
+    console.log(`%cDarkMode Forcer: Done! ${count}/${total} rules out of ${document.styleSheets.length} stylesheets processed.`, DMF_CSS);
 }
 
 start();
